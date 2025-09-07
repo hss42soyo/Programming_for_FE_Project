85f2a7885f88c889c8fadd7cf590c1a3c85dd9fe
hss42soyo <heshengsheng0112@163.com>
2025-09-07 16:07:54 -0400

    Update

--- High_Performance_Linear_Algebra_Kernels/Original_Linear_Operation.cpp
+++ High_Performance_Linear_Algebra_Kernels/Original_Linear_Operation.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include "Original_Linear_Operation.h"
 
+void OriginalLinearOperation::Verify(int cols,int rows){
+    if (cols != rows){
+        std::cout<<"Error: Matrix dimensions do not match!"<<std::endl;
+        exit(1);
+    }
+}
+
+void OriginalLinearOperation::Verify(int rowsA,int colsA,int rowsB,int colsB){
+    if (colsA != rowsB){
+        std::cout<<"Error: Matrix dimensions do not match!"<<std::endl;
+        exit(1);
+    }
+}
+
 void OriginalLinearOperation::multiply_mv_row_major(const double* matrix, int rows, int cols,
                                                   const double* vector, double* result) {
     for (int i = 0; i < rows; ++i) {
--- High_Performance_Linear_Algebra_Kernels/Test_Program.cpp
+++ High_Performance_Linear_Algebra_Kernels/Test_Program.cpp
@@ -1,15 +1,72 @@
 #include <iostream>
 #include <chrono>
+#include <random>
 #include "Original_Linear_Operation.h"
 
+// Size: small: 64x64, medium: 256x256, large: 1024x1024
+enum class MatrixSize {
+    TINY = 2,
+    SMALL = 64, 
+    MEDIUM = 256, 
+    LARGE = 1024
+};
+
+// Change these constants to test different sizes
+const int MATRIXSIZE = static_cast<int>(MatrixSize::TINY);
+const int VECTORSIZE = static_cast<int>(MatrixSize::TINY);
+const int MATRIXSIZEA = static_cast<int>(MatrixSize::TINY);
+const int MATRIXSIZEB = static_cast<int>(MatrixSize::TINY);
+
+ std::random_device rd;
+ std::mt19937 gen(rd());
+ std::uniform_int_distribution<> distrib(-100, 100);
+
+void CreateRandomMatrix_RowMajor(double* matrix, int rows, int cols) {
+    for (int i = 0; i < rows * cols; ++i) {
+        matrix[i] = static_cast<double>(distrib(gen));
+        //matrix[i] = i + 1;
+    }
+}
+void CreateRandomMatrix_ColMajor(double* matrix, int rows, int cols) {
+    for (int j = 0; j < cols; ++j) {
+        for (int i = 0; i < rows; ++i) {
+            matrix[i * cols + j] = static_cast<double>(distrib(gen));
+        }
+    }
+}
+void CreateRandomVector(double* vector, int size) {
+    for (int i = 0; i < size; ++i) {
+        vector[i] = static_cast<double>(distrib(gen));
+        //vector[i] = i + 1;
+    }
+}
+
 int main() {
-    // Instance for Matrix-Vector operations
-    const int rows1 = 1000;
-    const int cols1 = 1000;
-    double* matrix = new double[rows1 * cols1];
-    double* vector = new double[cols1];
-    double* result_row_major = new double[rows1];
-    double* result_col_major = new double[cols1];
+    OriginalLinearOperation originalOp;
+    // Initialize matrices and vectors
+    double* matrix_row = new double[MATRIXSIZE * MATRIXSIZE];
+    double* matrix_col = new double[MATRIXSIZE * MATRIXSIZE];
+    double* vector = new double[VECTORSIZE];
+    double* result_row_major = new double[MATRIXSIZE];
+    double* result_col_major = new double[MATRIXSIZE];
+
+    // Create random Matrix and Vector
+    CreateRandomMatrix_RowMajor(matrix_row, MATRIXSIZE, MATRIXSIZE);
+    CreateRandomMatrix_ColMajor(matrix_col, MATRIXSIZE, MATRIXSIZE);
+    CreateRandomVector(vector, VECTORSIZE);
+
+    // Verify dimensions for MV multiplication
+    originalOp.Verify(MATRIXSIZE,MATRIXSIZE);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    originalOp.multiply_mv_row_major(matrix_row, MATRIXSIZE, MATRIXSIZE, vector, result_row_major);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration_row_major = end - start;
+    std::cout << "Result (Row-Major MV): " << std::endl;
+    for (int i = 0; i < VECTORSIZE; ++i) {
+        std::cout << result_row_major[i] << " " << std::endl;
+    }
+    std::cout << "Row-Major MV Multiplication Time: " << duration_row_major.count() << " ms" << std::endl;
 
     // Instance for Matrix-Matrix operations
     const int rowsA = 1000;
@@ -25,7 +82,8 @@ int main() {
     
 
     // Release resources
-    delete[] matrix;
+    delete[] matrix_row;
+    delete[] matrix_col;
     delete[] vector;
     delete[] result_row_major;
     delete[] result_col_major;
