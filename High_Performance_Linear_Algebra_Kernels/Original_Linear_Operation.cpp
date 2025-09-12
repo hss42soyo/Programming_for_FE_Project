28304a29f602a57a480853ed075e96580e0d31a1
Ovenchan <cyxdontknow@gmail.com>
2025-09-12 13:36:26 -0400

    report and readme

--- High_Performance_Linear_Algebra_Kernels/Original_Linear_Operation.cpp
+++ High_Performance_Linear_Algebra_Kernels/Original_Linear_Operation.cpp
@@ -36,12 +36,13 @@ void OriginalLinearOperation::multiply_mv_col_major(const double* matrix, int ro
 }
 
 void OriginalLinearOperation::multiply_mm_naive(const double* matrixA, int rowsA, int colsA,
-                                                const double* matrixB, int rowsB, int colsB,double* result) {
-            for (int i = 0; i < rowsA; ++i) {
-                for (int j = 0; j < colsB; ++j) {
-                    result[i * colsB + j] = 0.0;
-                        for (int k = 0; k < colsA; ++k) {
-                            result[i * colsB + j] += matrixA[i * colsA + k] * matrixB[k * colsB + j];
+                                               const double* matrixB, int rowsB, int colsB,
+                                               double* result) {
+    for (int i = 0; i < rowsA; ++i) {
+        for (int j = 0; j < colsB; ++j) {
+            result[i * colsB + j] = 0.0;
+            for (int k = 0; k < colsA; ++k) {
+                result[i * colsB + j] += matrixA[i * colsA + k] * matrixB[k * colsB + j];
             }
         }
     }
@@ -49,9 +50,10 @@ void OriginalLinearOperation::multiply_mm_naive(const double* matrixA, int rowsA
 
 void OriginalLinearOperation::multiply_mm_transposed_b(const double* matrixA, int rowsA, int colsA,
                                                     const double* matrixB_transposed, int rowsB, int colsB, double* result) {
+    #pragma omp parallel for
     for (int i = 0; i < rowsA; ++i) {
         const double* arow = matrixA + i * colsA;  // i th row start pointer of A
-        double* crow = result + i * rowsB;         // i th row start pointer of result
+        double* crow = result + i * colsB;         // i th row start pointer of result
         for (int j = 0; j < colsB; ++j) {
             double sum = 0.0;
             // C[i,j] = sum_k A[i,k] * (B^T)[k,j]
